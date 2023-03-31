1730e65901724ba99644bdc672b7e3bdd8d56541
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-03-31 10:59:07 -0500

    comments

--- src/Components/EmojiCollection.js
+++ src/Components/EmojiCollection.js
@@ -3,6 +3,7 @@ import EmojiCard from "./EmojiCard";
 import { Card } from "semantic-ui-react";
 
 const EmojiCollection = ({ emojis, onHandleDelete }) => {
+  //creating a card for each emoji
   const renderOneEmoji = emojis.map((emoji, index) => {
     return (
       <EmojiCard key={index} emoji={emoji} onHandleDelete={onHandleDelete} />
